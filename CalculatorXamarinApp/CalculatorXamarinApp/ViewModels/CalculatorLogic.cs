7effc45f8d890323526d70a12814ca795d60a8a8
Jonathan <francisjlloyd15@gmail.com>
2023-04-16 13:07:19 +0200

    Add project files.

---
+++ CalculatorXamarinApp/CalculatorXamarinApp/ViewModels/CalculatorLogic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalculatorXamarinApp.ViewModels
+{
+    internal class CalculatorLogic : CalculatorLogicBase
+    {
+        // Class variables
+
+        // End
+        // Constructor
+        public CalculatorLogic()
+        {
+            buttonCalculate = new Command(ButtonCalculate);
+            buttonNumerical = new Command<string>((param) => ButtonNumPressed(param));
+        }
+        // End
+
+
+        // Methods
+        void ButtonCalculate()
+        {
+            CalcText = "-";
+        }
+
+        void ButtonNumPressed(string number)
+        {
+            if (CalcText == "-")
+            {
+                CalcText = number;
+            }
+            else
+            {
+                CalcText += number;
+            }
+        }
+        // End
+    }
+}
---
+++ CalculatorXamarinApp/CalculatorXamarinApp/ViewModels/CalculatorLogicBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalculatorXamarinApp.ViewModels
+{
+    internal class CalculatorLogicBase: BindableObject
+    {
+
+        // Properties
+        protected string calcText = "-";
+
+        public string CalcText
+        {
+            get { return calcText; }
+            set
+            {
+                if (calcText != value)
+                {
+                    calcText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        public Command buttonCalculate { set; get; }
+
+        public Command buttonNumerical { set; get; }
+        // End
+    }
+}
